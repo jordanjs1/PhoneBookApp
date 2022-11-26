b2af7fa1074c5735646ba930700f7f15915ac105
jordanjs1 <jordanlarson311@gmail.com>
2022-11-26 17:18:17 +0300

    Implement entites

---
+++ ContactMicroservice/Enums/ContactType.cs
@@ -0,0 +1,23 @@
+﻿namespace ContactMicroservice.Enums
+{
+    /// <summary>
+    /// Defines possible contact information types.
+    /// </summary>
+    public enum ContactInformationType
+    {
+        /// <summary>
+        /// Indicates that contact information is a mobile phone number.
+        /// </summary>
+        MobileNumber,
+
+        /// <summary>
+        /// Indicates that contact information is an e-mail address.
+        /// </summary>
+        EmailAddress,
+
+        /// <summary>
+        /// Indicates that contact information is a location.
+        /// </summary>
+        Location
+    }
+}
---
+++ ContactMicroservice/Models/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace ContactMicroservice.Models
+{
+    /// <summary>
+    /// Defines common fields among entities.
+    /// </summary>
+    /// <typeparam name="TId">The type of the unique identifier.</typeparam>
+    public class EntityBase<TId>
+    {
+        /// <summary>
+        /// The unique identifier.
+        /// </summary>
+        public TId Id { get; set; }
+    }
+}
